579f31d3c5845c5d096763980ae9e05dc70b88a8
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-03-26 22:24:20 +0900

    added .nojekyll & fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   output: "export",
   // GitHub Pages のリポジトリ名がユーザー名と同じ場合は basePath は不要
-  // basePath: '/bamtech1904.github.io',
+  basePath: "",
+  // 静的アセットのプレフィックスを設定
+  assetPrefix: "",
   images: {
     unoptimized: true,
   },
